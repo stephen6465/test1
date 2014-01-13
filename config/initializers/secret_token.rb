a804cd43515f23f9a4bd410685cd79a25a15eaa3
Stephen V. Wilson <stephen6465@gmail.com>
2014-01-13 02:11:26 -0500

    first ruby program
    first ruby program where I am learning how this all works.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Test1::Application.config.secret_key_base = 'a7c4cff31c9288e801b0072ad20a67fd2cfbd6b8e2b4bcb7b96bd2a872bd0d8c9d250bc4c34d6f23a1a5d0f810f39ee841fbaa9f9e5036a06b282f68ef2cab6d'
